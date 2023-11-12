016610eda1436d5f9c48d0933454a8c62e654b57
dreamjz <dreamjzwork@gmail.com>
2023-11-12 14:00:04 +0800

    fix(fix-legacy-linting-issues): modify statements
    Update codec.go

--- codec.go
+++ codec.go
@@ -112,7 +112,7 @@ func constructCodec(t reflect.Type, seen map[reflect.Type]*structType, canAddr b
 	}
 
 	if c.encode != nil {
-		return
+		return c
 	}
 
 	switch t.Kind() {
@@ -208,7 +208,7 @@ func constructCodec(t reflect.Type, seen map[reflect.Type]*structType, canAddr b
 		c.decode = constructTextUnmarshalerDecodeFunc(t, true)
 	}
 
-	return
+	return c
 }
 
 func constructStringCodec(t reflect.Type, seen map[reflect.Type]*structType, canAddr bool) codec {
@@ -795,13 +795,13 @@ func constructInlineValueEncodeFunc(encode encodeFunc) encodeFunc {
 //go:nosplit
 func noescape(p unsafe.Pointer) unsafe.Pointer {
 	x := uintptr(p)
-	return unsafe.Pointer(x ^ 0)
+	return unsafe.Pointer(x)
 }
 
 func alignedSize(t reflect.Type) uintptr {
 	a := t.Align()
 	s := t.Size()
-	return align(uintptr(a), uintptr(s))
+	return align(uintptr(a), s)
 }
 
 func align(align, size uintptr) uintptr {
@@ -1010,7 +1010,7 @@ func uintStringsAreSorted(u0, u1 uint64) bool {
 
 //go:nosplit
 func stringToBytes(s string) []byte {
-	return *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{ // nolint:govet // from segment's code
+	return *(*[]byte)(unsafe.Pointer(&reflect.SliceHeader{ //nolint:govet // from segment's code
 		Data: ((*reflect.StringHeader)(unsafe.Pointer(&s))).Data,
 		Len:  len(s),
 		Cap:  len(s),
